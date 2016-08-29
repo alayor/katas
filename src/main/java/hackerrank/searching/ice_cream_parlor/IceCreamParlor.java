dceb7426e7a3467f4ec9dcbe489582c71f6b996a
Alonso Ayala <alayor3@gmail.com>
2016-08-28 21:00:37 -0500

    Removing misleading tests and adding test files

--- src/main/java/hackerrank/searching/ice_cream_parlor/IceCreamParlor.java
+++ src/main/java/hackerrank/searching/ice_cream_parlor/IceCreamParlor.java
@@ -1,22 +1,11 @@
-/* Copyright 2016 Sabre Holdings */
 package hackerrank.searching.ice_cream_parlor;
 
-public class IceCreamParlor {
+class IceCreamParlor {
 
-    public int[] getBoughtIceCreams(int dollars, int[] iceCreamPrices) {
+    int[] getBoughtIceCreams(int dollars, int[] iceCreamPrices) {
         if (dollars < 2 || iceCreamPrices.length < 2) {
-            return new int[0];
+            return new int[] {0, 0};
         }
-
-        int[] iceCreamIndexes = new int[2];
-        int index = 0;
-        for (int i = 0; i < iceCreamPrices.length; i++) {
-            if(iceCreamPrices[i] > dollars)
-                continue;
-
-            iceCreamIndexes[index % 2] = i + 1;
-            index++;
-        }
-        return iceCreamIndexes;
+        return new int[] {0, 0};
     }
 }
