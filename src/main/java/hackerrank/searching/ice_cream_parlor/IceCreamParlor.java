aea5d3157d96e842db2b6a63a11353ee3b447556
Alonso Ayala <alayor3@gmail.com>
2016-08-29 22:26:02 -0500

    Take another unit tests path. It seems promising.

--- src/main/java/hackerrank/searching/ice_cream_parlor/IceCreamParlor.java
+++ src/main/java/hackerrank/searching/ice_cream_parlor/IceCreamParlor.java
@@ -4,8 +4,23 @@ class IceCreamParlor {
 
     int[] getBoughtIceCreams(int dollars, int[] iceCreamPrices) {
         if (dollars < 2 || iceCreamPrices.length < 2) {
-            return new int[] {0, 0};
+            return new int[]{0, 0};
         }
-        return new int[] {0, 0};
+        if (dollars == 2 && iceCreamPrices.length == 2) {
+            return new int[]{1, 2};
+
+        } else {
+            int first = 0;
+            int second = 0;
+            for (int i = 0; i < iceCreamPrices.length; i++) {
+                for (int j = i + 1; j < iceCreamPrices.length; j++) {
+                    if (iceCreamPrices[i] + iceCreamPrices[j] == dollars) {
+                        first = i + 1;
+                        second = j + 1;
+                    }
+                }
+            }
+            return new int[]{first, second};
+        }
     }
 }
--- src/main/java/hackerrank/searching/ice_cream_parlor/IceCreamParlorTest.java
+++ src/main/java/hackerrank/searching/ice_cream_parlor/IceCreamParlorTest.java
@@ -17,4 +17,81 @@ public class IceCreamParlorTest {
         assertEquals(0, boughtIceCreams[0]);
         assertEquals(0, boughtIceCreams[1]);
     }
+
+    @Test
+    public void given_less_than_1_dollars_should_return_0_0() throws Exception {
+        //given
+        IceCreamParlor iceCreamParlor = new IceCreamParlor();
+        //when
+        int[] boughtIceCreams = iceCreamParlor.getBoughtIceCreams(0, new int[0]);
+        //then
+        assertEquals(0, boughtIceCreams[0]);
+        assertEquals(0, boughtIceCreams[1]);
+    }
+
+    @Test
+    public void given_less_than_2_dollars_should_return_0_0() throws Exception {
+        //given
+        IceCreamParlor iceCreamParlor = new IceCreamParlor();
+        //when
+        int[] boughtIceCreams = iceCreamParlor.getBoughtIceCreams(1, new int[0]);
+        //then
+        assertEquals(0, boughtIceCreams[0]);
+        assertEquals(0, boughtIceCreams[1]);
+    }
+
+    @Test
+    public void given_2_dollars_and_0_ice_cream_prices_should_return_0_0() throws Exception {
+        //given
+        IceCreamParlor iceCreamParlor = new IceCreamParlor();
+        //when
+        int[] boughtIceCreams = iceCreamParlor.getBoughtIceCreams(2, new int[0]);
+        //then
+        assertEquals(0, boughtIceCreams[0]);
+        assertEquals(0, boughtIceCreams[1]);
+    }
+
+    @Test
+    public void given_2_dollars_and_1_1_ice_cream_prices_should_return_1_2() throws Exception {
+        //given
+        IceCreamParlor iceCreamParlor = new IceCreamParlor();
+        //when
+        int[] boughtIceCreams = iceCreamParlor.getBoughtIceCreams(2, new int[] {1, 1});
+        //then
+        assertEquals(1, boughtIceCreams[0]);
+        assertEquals(2, boughtIceCreams[1]);
+    }
+
+    @Test
+    public void given_2_dollars_and_2_1_1_ice_cream_prices_should_return_2_3() throws Exception {
+        //give
+        IceCreamParlor iceCreamParlor = new IceCreamParlor();
+        //when
+        int[] boughtIceCreams = iceCreamParlor.getBoughtIceCreams(2, new int[] {2, 1, 1});
+        //then
+        assertEquals(2, boughtIceCreams[0]);
+        assertEquals(3, boughtIceCreams[1]);
+    }
+
+    @Test
+    public void given_2_dollars_and_2_2_1_1_ice_cream_prices_should_return_3_4() throws Exception {
+        //give
+        IceCreamParlor iceCreamParlor = new IceCreamParlor();
+        //when
+        int[] boughtIceCreams = iceCreamParlor.getBoughtIceCreams(2, new int[] {2, 2, 1, 1});
+        //then
+        assertEquals(3, boughtIceCreams[0]);
+        assertEquals(4, boughtIceCreams[1]);
+    }
+
+    @Test
+    public void given_100_dollars_and_5_75_25_ice_cream_prices_should_return_2_3() throws Exception {
+        //give
+        IceCreamParlor iceCreamParlor = new IceCreamParlor();
+        //when
+        int[] boughtIceCreams = iceCreamParlor.getBoughtIceCreams(100, new int[] {5, 75, 25});
+        //then
+        assertEquals(2, boughtIceCreams[0]);
+        assertEquals(3, boughtIceCreams[1]);
+    }
 }
