a139660d6aba46eb84a0941ee97a959fa18c5dc1
Alonso Ayala <alayor3@gmail.com>
2016-09-18 12:47:25 -0500

    Add support to increase size of array

--- src/main/java/katas/algorithms/stacks_queues/queue_with_two_stacks/Stack.java
+++ src/main/java/katas/algorithms/stacks_queues/queue_with_two_stacks/Stack.java
@@ -2,20 +2,38 @@ package katas.algorithms.stacks_queues.queue_with_two_stacks;
 
 import java.util.Optional;
 
+import static java.lang.System.arraycopy;
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
 
 class Stack<T> {
-    private T[] value = (T[]) new Object[2];
-    int n;
+    private T[] array = createArray(1);
+    private int n;
+
+    private T[] createArray(int size) {
+        return (T[]) new Object[size];
+    }
 
     void push(T object) {
-        value[n++] = object;
+        array[n++] = object;
+        if (n == array.length) {
+            increaseArraySize(n * 2);
+        }
+    }
+
+    private void increaseArraySize(int newSize) {
+        T[] newArray = createArray(newSize);
+        arraycopy(array, 0, newArray, 0, array.length);
+        array = newArray;
     }
 
     Optional<T> pop() {
-        if(n < 1)
+        if (n < 1)
             return empty();
-        return of(value[--n]);
+        return of(array[--n]);
+    }
+
+    int getSize() {
+        return array.length;
     }
 }
--- src/main/java/katas/algorithms/stacks_queues/queue_with_two_stacks/StackTest.java
+++ src/main/java/katas/algorithms/stacks_queues/queue_with_two_stacks/StackTest.java
@@ -25,18 +25,24 @@ public class StackTest {
 
     @Test
     public void pop_should_return_pushed_value() {
+        //given
         stack.push(1);
+        //when
         Optional<Integer> value = stack.pop();
+        //then
         assertTrue(value.isPresent());
         assertEquals(1, value.get().intValue());
     }
 
     @Test
     public void pushed_values_should_be_kept() {
+        //given
         stack.push(1);
         stack.push(2);
+        //when
         Optional<Integer> value1 = stack.pop();
         Optional<Integer> value2 = stack.pop();
+        //then
         assertTrue(value1.isPresent());
         assertTrue(value2.isPresent());
         assertEquals(2, value1.get().intValue());
@@ -45,7 +51,53 @@ public class StackTest {
 
     @Test
     public void given_empty_stack_pop_should_return_not_present() {
+        //when
         Optional<Integer> value = stack.pop();
+        //then
         assertFalse(value.isPresent());
+    }
+
+    @Test
+    public void given_empty_stack_then_size_should_be_0() {
+        assertEquals(1, stack.getSize());
+    }
+
+    @Test
+    public void given_1_push_then_size_should_be_2() {
+        //when
+        stack.push(1);
+        //then
+        assertEquals(2, stack.getSize());
+    }
+
+    @Test
+    public void given_2_pushes_then_size_should_be_4() {
+        //when
+        stack.push(1);
+        stack.push(2);
+        //then
+        assertEquals(4, stack.getSize());
+    }
+
+    @Test
+    public void given_3_pushes_then_size_should_be_6() {
+        //when
+        pushTimes(3);
+        //then
+        assertEquals(4, stack.getSize());
+    }
+
+    @Test
+    public void given_4_pushes_then_size_should_be_8() {
+        //when
+        pushTimes(4);
+        //then
+        assertEquals(8, stack.getSize());
+    }
+
+    private void pushTimes(int times) {
+        while (times-- > 0) {
+            stack.push(times);
+        }
     }
 }
